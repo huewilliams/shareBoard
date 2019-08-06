fa5cc66d174a762f73618c95a624a7a410944eaa
huewilliams <4879sinwoo@naver.com>
2019-08-07 00:04:05 +0900

    apply docs router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const sequelize = require('./models').sequelize;
 const classRouter = require('./routes/class');
 const taskRouter = require('./routes/task');
 const authRouter = require('./routes/auth');
+const docsRouter = require('./routes/document');
 
 const app = express();
 sequelize.sync();
@@ -19,6 +20,7 @@ app.use(express.urlencoded({extended: false, limit: '50mb'}));
 app.use('/class', classRouter);
 app.use('/task', taskRouter);
 app.use('/auth', authRouter);
+app.use('/docs', docsRouter);
 
 app.listen(app.get('port'), ()=> {
     console.log(`server listening on ${app.get('port')}`)
