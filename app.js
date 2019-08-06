79bdd02fe1e89549249d5706d36cbfaa2f253b97
huewilliams <4879sinwoo@naver.com>
2019-08-07 02:35:53 +0900

    setting static access

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config({path: '.env'});
 
 const sequelize = require('./models').sequelize;
@@ -19,6 +20,7 @@ app.set('port', 5000);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false, limit: '50mb'}));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/class', classRouter);
 app.use('/task', taskRouter);
