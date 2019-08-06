8944c943444ae953f2ef78e6fb1ee7bba860e742
huewilliams <4879sinwoo@naver.com>
2019-08-06 23:53:18 +0900

    create document relation

--- models/index.js
+++ models/index.js
@@ -18,8 +18,15 @@ db.Sequelize = Sequelize;
 db.Class = require('./Class')(sequelize, Sequelize);
 db.Task = require('./Task')(sequelize, Sequelize);
 db.Student = require('./Student')(sequelize, Sequelize);
+db.Document = require('./Document')(sequelize, Sequelize);
 
 db.Class.hasMany(db.Task, { foreignKey: 'className', sourceKey: 'name'});
 db.Task.belongsTo(db.Class, { foreignKey: 'className', targetKey: 'name'});
+
+db.Student.hasMany(db.Document, { foreignKey: 'author', sourceKey: 'id'});
+db.Document.belongsTo(db.Student, { foreignKey: 'author', targetKey: 'id'});
+
+db.Task.hasMany(db.Document, { foreignKey: 'task', sourceKey: 'taskId'});
+db.Document.belongsTo(db.Task, { foreignKey: 'task', targetKey: 'taskId'});
 
 module.exports = db;
