17410579e1c4c7a6670488ef7305de81f309f97f
huewilliams <4879sinwoo@naver.com>
2019-08-06 19:54:02 +0900

    create task schema

---
+++ models/Task.js
@@ -0,0 +1,57 @@
+const uuid = require('uuid/v4');
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Task', {
+        taskId: {
+            type: DataTypes.TEXT,
+            defaultValue: uuid(),
+        },
+        taskName: {
+            type: DataTypes.STRING(30),
+            required: true,
+        },
+        taskStart: {
+            type: DataTypes.DATE,
+            defaultValue: getDate(),
+        },
+        taskEndLine: {
+            type: DataTypes.DATE,
+            required: true,
+        },
+        taskInfo : {
+            type: DataTypes.TEXT,
+            required: true,
+        },
+        file : {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        }
+    },{
+        timestamps: false,
+    })
+};
+
+function getDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    if(month < 10) {
+        month = "0"+month;
+    }
+    if(day < 10) {
+        day = "0"+day;
+    }
+    if(hours < 10) {
+        hours = "0"+hours;
+    }
+    if(minutes < 10) {
+        minutes = "0"+minutes;
+    }
+
+    const result = `${year}-${month}-${day} ${hours}:${minutes}`;
+    return result;
+}
